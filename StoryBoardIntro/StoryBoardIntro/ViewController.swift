b98ae0a17ebc09ddbab1bc2a8732321f06971aef
sunzanalima <68555619+sunzanalima@users.noreply.github.com>
2020-07-22 11:49:13 -0400

    KWK practice Xcode run

---
+++ StoryBoardIntro/StoryBoardIntro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoryBoardIntro
+//
+//  Created by Sunzana Lima on 7/22/20.
+//  Copyright © 2020 KWK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("This is a print statement")
+    }
+
+
+}
+
